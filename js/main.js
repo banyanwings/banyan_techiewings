39e6b2be231b0d9e943da28875410f0ac1543828
banyanwings <127676795+banyanwings@users.noreply.github.com>
2023-04-06 16:56:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -133,7 +133,7 @@ var x = setInterval(function() {
 }, 1000);
 
 // Set the date we're counting down to
-var countDownDate1 = new Date("Apr 08, 2023").getTime();
+var countDownDate1 = new Date("Apr 22, 2023").getTime();
 
 // Update the count down every 1 second
 var y = setInterval(function() {
@@ -163,7 +163,7 @@ var y = setInterval(function() {
 
 
 // Set the date we're counting down to
-var countDownDate2 = new Date("Apr 15, 2023").getTime();
+var countDownDate2 = new Date("Apr 29, 2023").getTime();
 
 // Update the count down every 1 second
 var z = setInterval(function() {
@@ -193,7 +193,7 @@ var z = setInterval(function() {
 
 
 // Set the date we're counting down to
-var countDownDate3 = new Date("Apr 21, 2023").getTime();
+var countDownDate3 = new Date("May 05, 2023").getTime();
 
 // Update the count down every 1 second
 var x1 = setInterval(function() {
